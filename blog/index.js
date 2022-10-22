82648590589248552c68f93d3c4bb4b9ef90cf9d
liabri <liabri@tuta.io>
2022-10-22 10:59:19 +0200

    blog successful cross-page theme using JavaScript

--- blog/index.js
+++ blog/index.js
@@ -1,13 +1,17 @@
 // Remember theme selection
 const themeSwitch = document.querySelector('.theme-switch');
-themeSwitch.checked = localStorage.getItem('switchedTheme') === 'true';
+themeSwitch.checked = localStorage.getItem('darkTheme') === 'true';
+
+// if localStorage.getItem('darkTheme') === 'true' {
+// 	document.querySelector('.theme-switch').checked = true;
+// }
 
 themeSwitch.addEventListener('change', function (e) {
   if(e.currentTarget.checked === true) {
     // Add item to localstorage
-    localStorage.setItem('switchedTheme', 'true');
+    localStorage.setItem('darkTheme', 'true');
   } else {
     // Remove item if theme is switched back to normal
-    localStorage.removeItem('switchedTheme');
+    localStorage.removeItem('darkTheme');
   }
 });
