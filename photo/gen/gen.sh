2dd278972761b89ff5cb5d33a6c5e812af4777eb
liabri <liabri@tuta.io>
2024-01-03 01:12:56 +0100

    photos fix links n stuff

---
+++ photo/gen/gen.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Generate site with https://github.com/liabri/lenti
+
+# input is original photo library
+# output is site
+# resources is templates and stuff
+./lenti --input /home/liabri/ritratti/fotografija/librerija/ --output .. --resources $PWD
