2ed3bc39081de848485456958fffb4f8245c66df
liabri <liabri@tuta.io>
2026-03-19 20:27:20 +0100

    swapped up landing page + css vars

--- index.js
+++ index.js
@@ -1,46 +1,12 @@
 let scrollEnabled = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-    // landing page logic
-    const landing = document.getElementById("landing");
-    const content = document.getElementById("content");
+    window.addEventListener('hashchange', () => {
+      if (window.location.hash.length > 1) {
+        document.documentElement.setAttribute('data-has-hash', 'true');
+      }
+    });
 
-    // if (!sessionStorage.getItem('hasVisited')) {
-
-        // prevent scrolling in section until entered site
-        if (typeof fullpage_api !== 'undefined') {
-            fullpage_api.setAutoScrolling(false);
-            fullpage_api.setKeyboardScrolling(false);
-        }
-        function enterSite() {
-            content.style.display = "block";
-            document.body.classList.add("site-entered");
-
-            // opens section specified in url
-            if (typeof fullpage_api !== 'undefined') {
-                fullpage_api.setAutoScrolling(true);
-                fullpage_api.setKeyboardScrolling(true);
-            }
-
-            // after the transition finishes, hide the landing page
-            landing.addEventListener('transitionend', () => {
-                landing.style.display = 'none';
-                scrollEnabled = true;
-            });
-        }
-
-        // enter on click, scroll or key press
-        const excludedKeys = ['Control', 'Alt', 'Shift', 'Meta'];
-        window.addEventListener("keydown", (e) => { if (!excludedKeys.includes(e.key)) { enterSite(); }}, { once: true });
-        landing.addEventListener("click", enterSite, { once: true });
-        window.addEventListener("wheel", enterSite, { once: true });
-    // } else {
-    //     isTransitionLocked = false;
-    //     enable_fullpage_scroll();
-    //     scrollEnabled = true;
-    //     content.style.display = "block";
-    //     landing.style.display = 'none';
-    // }
 
     // disable select features on non mouse devices
     window.addEventListener("load", function () {
@@ -106,14 +72,6 @@ document.addEventListener("DOMContentLoaded", () => {
                     setTimeout(() => {
                         projectsElement.style.transition = ''; // re-add
                     }, 100);
-
-                    // match nav to section
-                    const section = document.querySelector('.fp-section.active'); // currently active section
-                    const bgSource = section.querySelector('[id$="Canvas"]'); // try to find the first div ending with 'Canvas' inside the section
-                    const nav = document.querySelector('nav ul');
-                    nav.style.background = getComputedStyle(bgSource).background;
-                    nav.style.color = "";
-                    nav.style.borderColor = "";
                 }
             }, 350);
 
@@ -125,11 +83,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 isTransitionLocked = false;
             }, 1300); // grace period to absorb leftover scroll events and wait for transition
 
-            // match nav to section
-            const section = destination.item; // currently active section
-            const bgSource = section.querySelector('[id$="Canvas"]'); // try to find the first div ending with 'Canvas' inside the section
-            const nav = document.querySelector('nav ul');
-            nav.style.background = getComputedStyle(bgSource).background;
+            // update --section-bg var to the currently active section
+            const section = document.querySelector('.fp-section.active');
+            document.documentElement.style.setProperty('--section-bg', "rgb("+section.dataset.color+")");
+
+            // make sure --nav-bg is the same as --section-bg
+            document.documentElement.style.setProperty('--nav-bg', 'var(--section-bg)');
+            document.documentElement.style.setProperty('--nav-text', 'var(--text-primary)');
+
         }
     });
 
@@ -187,11 +148,11 @@ document.addEventListener("DOMContentLoaded", () => {
                         fullpage_api.moveSectionDown();
                     }
                 }
-                // If a move is not possible (e.g., scrolling up on the first section),
+                // if a move is not possible (e.g., scrolling up on the first section),
                 // we simply do nothing, and the scroll is ignored, preventing a lock.
 
             } else {
-                // Perform horizontal scroll
+                // perform horizontal scroll
                 sectionInner.targetScrollX += e.deltaY;
                 sectionInner.targetScrollX = Math.max(0, Math.min(sectionInner.targetScrollX, sectionInner.scrollWidth - sectionInner.clientWidth));
 
@@ -204,25 +165,25 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 
-     // A single, generic animation function
+     // a single, generic animation function
      function smoothScroll(sectionInner) {
-         // Stop if the sectionInner is no longer active or the animation flag is turned off, and set section back to beginning
+         // stop if the sectionInner is no longer active or the animation flag is turned off, and set section back to beginning
          if (!sectionInner.closest('.fp-section.active') || !sectionInner.isScrolling) {
              sectionInner.isScrolling = false;
              return;
          }
 
-         // Lerp logic
+         // lerp logic
          sectionInner.currentScrollX += (sectionInner.targetScrollX - sectionInner.currentScrollX) * 0.1;
          sectionInner.scrollLeft = sectionInner.currentScrollX;
 
-         // Stop when we're close enough to the target
+         // stop when we're close enough to the target
          if (Math.abs(sectionInner.targetScrollX - sectionInner.currentScrollX) < 0.5) {
              sectionInner.isScrolling = false;
              return;
          }
 
-         // Continue the animation
+         // continue the animation
          requestAnimationFrame(() => smoothScroll(sectionInner));
      }
 });
@@ -245,21 +206,16 @@ function show(self) {
     if (isTransitionLocked) return;
     const nav = document.querySelector('nav ul');
 
-    // if (about) {
     if (self.id=="about-button") {
-        const close = document.querySelector('#about .close');
-        nav.style.backgroundColor = "rgba(255, 254, 237, 0.9)";
-        close.style.backgroundColor = "rgba(255, 254, 237, 0.9)";
+        // --nav-bg is off white when abouting
+        document.documentElement.style.setProperty('--nav-bg', 'var(--bg-primary)');
+        document.documentElement.style.setProperty('--nav-text', 'var(--text-primary)');
         document.getElementById("about").classList.add("active");
     }
     else if (self.id=="explore-button") {
-        nav.style.backgroundColor = "#090909";
-        nav.style.borderColor= "rgba(255, 254, 237, 0.9)";
-        nav.style.color = "rgba(255, 254, 237, 0.9)";
-
-        const close = document.querySelector('#projects .close');
-        close.style.backgroundColor = "#090909";
-
+        // --nav-bg is dark grey when exploring
+        document.documentElement.style.setProperty('--nav-bg', 'var(--bg-secondary)');
+        document.documentElement.style.setProperty('--nav-text', 'var(--text-secondary)');
         document.getElementById("projects").classList.add("active");
     }
 
@@ -273,10 +229,6 @@ function hide() {
     document.getElementById("projects").classList.remove("active");
 
     // match nav to section
-    const section = document.querySelector('.fp-section.active'); // currently active section
-    const bgSource = section.querySelector('[id$="Canvas"]'); // try to find the first div ending with 'Canvas' inside the section
-    const nav = document.querySelector('nav ul');
-    nav.style.background = getComputedStyle(bgSource).background;
-    nav.style.color = "";
-    nav.style.borderColor = "";
+    document.documentElement.style.setProperty('--nav-bg', 'var(--section-bg)');
+    document.documentElement.style.setProperty('--nav-text', 'var(--text-primary)');
 }
